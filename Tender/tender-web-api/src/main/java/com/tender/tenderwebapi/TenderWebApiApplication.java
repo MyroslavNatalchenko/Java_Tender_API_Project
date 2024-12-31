b3faa5b96ffe5c2e02f3efad2736a12aadef44bc
Миря <miroslav.natalchenko@gmail.com>
2024-12-31 20:11:02 +0100

    Web Api module start

---
+++ Tender/tender-web-api/src/main/java/com/tender/tenderwebapi/TenderWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.tender.tenderwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenderWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TenderWebApiApplication.class, args);
+    }
+
+}
